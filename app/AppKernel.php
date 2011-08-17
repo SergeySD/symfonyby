3b0675fca3f2d8884fddfbf86f3036cdc9b34cc1
Sergey Stepanov <sergey@sadgroup.com>
2011-08-17 23:32:52 +0300

    configuring twitter bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Sfby\StaticBundle\SfbyStaticBundle(),
+            new FOS\TwitterBundle\FOSTwitterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
