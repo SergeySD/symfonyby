f5024c6414bf9c38cfb281272124447ce22afda9
michail zuev <michail.zuev@sadgroup.com>
2011-11-25 19:44:35 +0300

    likes

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,23 @@
-function disqusVote(vote, id)
+$(document).ready(function(){
+    var url = 'https://disqus.com/api/3.0/threads/list.jsonp?forum=symfonyby&callback=updateVotes&api_key='+disqus_public_key;
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = url;
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+});
+
+function updateVotes(data)
 {
-    var url = 'https://disqus.com/api/3.0/threads/vote.json?api_key='+disqus_public_key;
-    $.post(url, {vote: vote, "thread:ident": id, forum: "symfonyby"}, function (res)
+    var res = data.response;
+    for (i in res)
     {
-        
-    }, "json");
+        var cnt = res[i].likes - res[i].dislikes;
+        var id = res[i].identifiers[0];
+        console.log(cnt)
+        console.log(id)
+        if ($('#likes'+id))
+            $('#likes'+id).html(cnt);
+    }
 }
