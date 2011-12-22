61098b89fd48ab979191291603fce736a52a1b7a
Sergey Stepanov <sergey@sadgroup.com>
2011-12-22 18:09:49 +0200

    added confirmation and resetting

---
+++ src/Sfby/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sfby\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\RegistrationController as Controller;
+/**
+ * Controller managing the registration
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class RegistrationController extends Controller
+{
+ 
+}
